cb05c1563dc2fceb998a9f6b12743ecd44ca489a
AnhuarRuiz <anhuar.me@outlook.com>
2026-03-06 17:12:27 -0600

    update trash

--- src/js/main.js
+++ src/js/main.js
@@ -107,73 +107,6 @@
 
     window.addEventListener('load', aosInit);
 
-    /**
-     * Countdown timer
-     */
-    function updateCountDown(countDownItem) {
-        const timeleft = new Date(countDownItem.getAttribute('data-count')).getTime() - new Date().getTime();
-
-        const days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-
-        const daysElement = countDownItem.querySelector('.count-days');
-        const hoursElement = countDownItem.querySelector('.count-hours');
-        const minutesElement = countDownItem.querySelector('.count-minutes');
-        const secondsElement = countDownItem.querySelector('.count-seconds');
-
-        if (daysElement) daysElement.innerHTML = days;
-        if (hoursElement) hoursElement.innerHTML = hours;
-        if (minutesElement) minutesElement.innerHTML = minutes;
-        if (secondsElement) secondsElement.innerHTML = seconds;
-
-    }
-
-    document.querySelectorAll('.countdown').forEach(function (countDownItem) {
-        updateCountDown(countDownItem);
-        setInterval(function () {
-            updateCountDown(countDownItem);
-        }, 1000);
-    });
-
-    /**
-     * Initiate Pure Counter
-     */
-    new PureCounter();
-
-    /**
-     * Init isotope layout and filters
-     */
-    document.querySelectorAll('.isotope-layout').forEach(function (isotopeItem) {
-        let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
-        let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
-        let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
-
-        let initIsotope;
-        imagesLoaded(isotopeItem.querySelector('.isotope-container'), function () {
-            initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
-                itemSelector: '.isotope-item',
-                layoutMode: layout,
-                filter: filter,
-                sortBy: sort
-            });
-        });
-
-        isotopeItem.querySelectorAll('.isotope-filters li').forEach(function (filters) {
-            filters.addEventListener('click', function () {
-                isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
-                this.classList.add('filter-active');
-                initIsotope.arrange({
-                    filter: this.getAttribute('data-filter')
-                });
-                if (typeof aosInit === 'function') {
-                    aosInit();
-                }
-            }, false);
-        });
-
-    });
 
     document.addEventListener('DOMContentLoaded', function () {
         const nameElements = document.querySelectorAll('.footer .ticket-card .ticket-header .ticket-icon h3');
@@ -188,62 +121,6 @@
         });
     });
 
-
-    /*
-     * Pricing Toggle
-     */
-
-    const pricingContainers = document.querySelectorAll('.pricing-toggle-container');
-
-    pricingContainers.forEach(function (container) {
-        const pricingSwitch = container.querySelector('.pricing-toggle input[type="checkbox"]');
-        const monthlyText = container.querySelector('.monthly');
-        const yearlyText = container.querySelector('.yearly');
-
-        pricingSwitch.addEventListener('change', function () {
-            const pricingItems = container.querySelectorAll('.pricing-item');
-
-            if (this.checked) {
-                monthlyText.classList.remove('active');
-                yearlyText.classList.add('active');
-                pricingItems.forEach(item => {
-                    item.classList.add('yearly-active');
-                });
-            } else {
-                monthlyText.classList.add('active');
-                yearlyText.classList.remove('active');
-                pricingItems.forEach(item => {
-                    item.classList.remove('yearly-active');
-                });
-            }
-        });
-    });
-
-    /**
-     * Init swiper sliders
-     */
-    function initSwiper() {
-        document.querySelectorAll(".init-swiper").forEach(function (swiperElement) {
-            let config = JSON.parse(
-                swiperElement.querySelector(".swiper-config").innerHTML.trim()
-            );
-
-            if (swiperElement.classList.contains("swiper-tab")) {
-                initSwiperWithCustomPagination(swiperElement, config);
-            } else {
-                new Swiper(swiperElement, config);
-            }
-        });
-    }
-
-    window.addEventListener("load", initSwiper);
-
-    /**
-     * Initiate glightbox
-     */
-    const glightbox = GLightbox({
-        selector: '.glightbox'
-    });
 
     /**
      * Correct scrolling position upon page load for URLs containing hash links.
